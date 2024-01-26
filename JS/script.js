e286a3dd55263d2ab453dff3f112f7d8c8459b9d
Sima <didkovskayasima@gmail.com>
2024-01-26 17:03:27 +0200

    personalMovieDB

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,23 @@
-'use strict';
+//'use strict';
 
-console.log('arr'+ '-object');
-console.log(4 + +'5');
+//console.log('arr'+ '-object');
+//console.log(4 + +'5');
 
-let incr = 10,
-	decr = 10;
+//let incr = 10,
+	//decr = 10;
 //++incr;
 //--decr;
 
-console.log(++incr);
-console.log(--decr);
+//console.log(++incr);
+//console.log(--decr);
 
-console.log(5%2);
+//console.log(5%2);
 
-console.log(2 + 2 * 2 !== '6');
+//console.log(2 + 2 * 2 !== '6');
 
-const	isChecked = false;
-		isClose = false;
-
-console.log(isChecked || !isClose);
+//const	isChecked = false;
+		//isClose = false;
+//console.log(isChecked || !isClose);
 //const category = 'toys';
 
 //console.log(`hhtps://someurl/${category}/5`);
@@ -43,3 +42,23 @@ console.log(isChecked || !isClose);
 
 //console.log(typeof(answers));
 //console.log(typeof(null));
+
+const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?','');
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const	a = prompt ('Один из последних просмотренных фильмов?', ''),
+		b = prompt ('На сколько оцените его?', ''),
+		c = prompt ('Один из последних просмотренных фильмов?', ''),
+		d = prompt ('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
